67ca9f127bd4e36f0fefd2826c5094025d35fed7
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2023-04-16 23:34:39 +0200

    chore: add renderer for a given solution in the solitaire game

---
+++ src/main/scala/u06lab/code/Solitaire.scala
@@ -0,0 +1,15 @@
+package u06lab.code
+
+object Solitaire extends App:
+  def render(solution: Seq[(Int, Int)], width: Int, height: Int): String =
+    val reversed = solution.reverse
+    val rows =
+      for y <- 0 until height
+          row = for x <- 0 until width
+          number = reversed.indexOf((x, y)) + 1
+          yield if number > 0 then "%-2d ".format(number) else "X  "
+      yield row.mkString
+    rows.mkString("\n")
+
+
+  println(render(solution = Seq((0, 0), (2, 1)), width = 3, height = 3))
